da113a9b7a56e2a3ff17a7d0ee7ebbf91e94a002
Jere <jere@flamero.fi>
2021-10-29 15:56:42 +0300

    Watch for price changes

--- tori.py
+++ tori.py
@@ -2,9 +2,14 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
 from datetime import datetime
-import sched, time, requests
+import sched, time, requests, re
 
 load_dotenv()
+
+class item:
+    def __init__(self, ID, price):
+        self.ID = ID
+        self.price = price
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
@@ -13,22 +18,37 @@ URL = 'https://www.tori.fi/koko_suomi?q='+KEYWORD.replace(' ', '+')+'&st=s'
 found = []
 s = sched.scheduler(time.time, time.sleep)
 
+found.append(item("item_89405600", 1400))
+
 def fetchTori(sc,init):
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
     results = soup.find('div', class_='list_mode_thumb')
     try:
         elements = results.find_all('a', class_='item_row_flex')
+        if not elements:
+            print(datetime.now().time(), "No results")
+            return
         for element in elements:
             id = element['id']
             link = element['href']
-            if id not in found:
-                found.append(id)
+            price = re.sub("[^0-9]", "", element.find('div', class_='desc_flex') \
+                           .find('div', class_='ad-details-left') \
+                           .find('div', class_='list-details-container') \
+                           .find('p', class_='list_price').contents[0])
+            if not any(x for x in found if x.ID == id):
+                found.append(item(id, price))
                 print(datetime.now().time(), "Found new listing", link)
                 if not init:
-                    postTelegram(link)
-    except:
-        print(datetime.now().time(), "No results")
+                    postTelegram("Found new listing\n"+link)
+            elif any(x for x in found if x.ID == id and x.price != price):
+                for x in found:
+                    if x.ID == id:
+                        print(datetime.now().time(), "Price changed for item")
+                        postTelegram("Price changed for item from "+str(x.price)+"€"+" to "+price+"€"+"\n"+link)
+                        x.price = price
+    except Exception as e:
+        print(datetime.now().time(), "ERROR", e)
     s.enter(300, 1, fetchTori, (sc,False,))
 
 def postTelegram(message):
